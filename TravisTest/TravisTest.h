765c31689d2e89706ce5ff72cae9e00e47b28bb8
ShezHsky <shezhsky@icloud.com>
2016-07-26 21:54:01 +0100

    Initial commit

---
+++ TravisTest/TravisTest.h
@@ -0,0 +1,19 @@
+//
+//  TravisTest.h
+//  TravisTest
+//
+//  Created by Thomas Sherwood on 26/07/2016.
+//  Copyright © 2016 ShezHsky. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for TravisTest.
+FOUNDATION_EXPORT double TravisTestVersionNumber;
+
+//! Project version string for TravisTest.
+FOUNDATION_EXPORT const unsigned char TravisTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TravisTest/PublicHeader.h>
+
+
---
+++ TravisTestTests/TravisTestTests.swift
@@ -0,0 +1,18 @@
+//
+//  TravisTestTests.swift
+//  TravisTestTests
+//
+//  Created by Thomas Sherwood on 26/07/2016.
+//  Copyright © 2016 ShezHsky. All rights reserved.
+//
+
+import XCTest
+@testable import TravisTest
+
+class TravisTestTests: XCTestCase {
+    
+    func testSomething() {
+        
+    }
+    
+}
